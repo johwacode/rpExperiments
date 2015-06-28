2049dfa8c7389d216430a707f994833a6fcbc589
Johannes <johannes_wandsleb@freenet.de>
2015-06-28 09:28:17 +0200

    some basic stuff for creating CollisionEvents

--- src/rpEngine/graphical/model/CollisionEvent.java
+++ src/rpEngine/graphical/model/CollisionEvent.java
@@ -1,5 +1,25 @@
 package rpEngine.graphical.model;
 
-public class CollisionEvent {
+import java.util.LinkedList;
+import java.util.List;
 
+import utils.math.Vector3f;
+
+public class CollisionEvent {
+	private List<CollisionBox> collidingBoxes;
+	private List<Vector3f> collisionSpots;
+	
+	public CollisionEvent(){
+		collidingBoxes = new LinkedList<>();
+		collisionSpots = new LinkedList<>();
+	}
+	
+	public void addData(CollisionBox collisionBox){
+		collidingBoxes.add(collisionBox);
+	}
+	public void addData(Vector3f collisionSpot){
+		collisionSpots.add(collisionSpot);
+	}
+	
+	//....
 }
